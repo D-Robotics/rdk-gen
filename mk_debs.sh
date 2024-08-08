d424e393b3a715a5e137330191e5cd0525a15197
xin03.liu <xin03.liu@horizon.cc>
2024-08-08 17:19:27 +0800

    feat(mk_debs): Change source from HorizonRDK to D-Robotics,add feat-ubuntu22.04 for source_sync
    Signed-off-by: xin03.liu <xin03.liu@horizon.cc>

--- mk_debs.sh
+++ mk_debs.sh
@@ -496,7 +496,7 @@ function make_debian_deb() {
 
 deb_pkg_list=(
     "hobot-boot"
-    "hobot-kernel-headers"
+    #"hobot-kernel-headers"
     "hobot-dtb"
     "hobot-bpu-drivers"
     "hobot-configs"
